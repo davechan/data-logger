4996e84a333dbf4a3a3f8167f48898be7badf446
Dave Chan <davechan@users.noreply.github.com>
2021-03-21 22:31:20 +0800

    add distance

--- main.ts
+++ main.ts
@@ -5,8 +5,13 @@ radio.onReceivedValue(function (name, value) {
         y = value
     } else if (name == "z") {
         z = value
+    } else if (name == "d") {
+        d = value
+    } else {
+    	
     }
 })
+let d = 0
 let z = 0
 let y = 0
 let x = 0
@@ -15,4 +20,5 @@ basic.forever(function () {
     serial.writeValue("x", x)
     serial.writeValue("y", y)
     serial.writeValue("z", z)
+    serial.writeValue("d", d)
 })
